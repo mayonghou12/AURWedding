bc3b0aed8c9242e16fdcfe9976791307ec2f767b
ImHeXiaoP <2698420775@qq.com>
2020-06-17 17:07:15 +0800

    酒店，婚纱，蜜月页面及优化

--- src/Public/HoneyMoon/HMson/index.vue
+++ src/Public/HoneyMoon/HMson/index.vue
@@ -5,7 +5,7 @@
             <div class="dress-c-title">
                 <h3>
                     <a href="">
-                        婚纱
+                        蜜月
                     </a>
                     <em></em>
                 </h3>
@@ -14,15 +14,15 @@
             <div class="dress-content">
                 <!-- 婚纱左边内容开始 -->
                 <div class="content-left">
-                    <div class="content-left-title"><h3>2016秋冬苏妃(3)</h3></div>
+                    <div class="content-left-title"><h3>蜜月(3)</h3></div>
                     <!-- 图片内容 -->
                     <div class="content-left-img">
-                       <img src="http://pic.tdy.picdns.com/52-0146/show/201709/14/104833QsjEk.jpg" alt="">
+                       <img src="http://pic.tdy.picdns.com/52-0146/show/201709/08/182313GvItK.jpg" alt="">
                     </div>
                     <!-- 翻页开始 -->
                     <div class="fanye">
-                        <p>上一篇：<a href="">2016秋冬苏妃(2)</a></p>
-                        <p>下一篇：<a href="">2016春夏奈特丽(2)</a></p>
+                        <!-- <p>上一篇：<a href="">2016秋冬苏妃(2)</a></p> -->
+                        <p>下一篇：<a href="">蜜月01</a></p>
                     </div>
                 </div>
                 <!-- 婚纱右边内容开始 -->
@@ -34,5 +34,5 @@
         </div>
     </div>
 </template>
-<style src='../../Common/CSS/dress.css'>
+<style src='../../Common/CSS/honeymoon.css'>
 </style>
